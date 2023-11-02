31880e45d9901787fb1c19ae91b58bac7385116b
marziamostafa <marziamostafa@gmail.com>
2023-11-02 13:53:50 +0600

    banner picture added

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,6 +1,8 @@
 
 
 
+import img1 from "../../assets/banner/Gadgets.webp"
+import img2 from "../../assets/banner/photo-1515940175183-6798529cb860.avif"
 
 
 
@@ -9,31 +11,30 @@ const Banner = () => {
 
 
     return (
+
         <div>
-            <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" />
+            <div className="carousel w-full">
+                <div id="item1" className="carousel-item w-full">
+                    <img src={img1} className="w-full h-screen" />
                 </div>
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" />
+                <div id="item2" className="carousel-item w-full">
+                    <img src={img2} className="w-full h-screen" />
                 </div>
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="rounded-box" />
+                <div id="item3" className="carousel-item w-full">
+                    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
                 </div>
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1494253109108-2e30c049369b.jpg" className="rounded-box" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="rounded-box" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1559181567-c3190ca9959b.jpg" className="rounded-box" />
-                </div>
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" />
+                <div id="item4" className="carousel-item w-full">
+                    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
                 </div>
             </div>
+            <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
+                <a href="#item4" className="btn btn-xs">4</a>
+            </div>
         </div>
+
 
     );
 };
