1ceaf7c85faefb5c44b6385d683a871e19bf9930
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-11-03 20:47:05 +0600

    brand name shown

--- src/components/Shared/Navbar/NavbarItem.tsx
+++ src/components/Shared/Navbar/NavbarItem.tsx
@@ -7,8 +7,11 @@ export default function NavbarItem() {
     const { data, isLoading } = useApiData("http://localhost:5000/api/v1/allProducts")
 
     // Extract unique category,subcategory and brands name
+
     const categories = [...new Set(data.map((item) => item.category_name))];
+
     console.log(categories)
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -21,16 +24,16 @@ export default function NavbarItem() {
         ),
     ];
     console.log(Brands)
-
-
     return (
-        <div>
+        <div className='border-2 flex justify-evenly'>
             {
-                categories.map((category) =>
-                    <div className="dropdown">
-                        <button className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded ">
-                            {category}
-                        </button>
+             categories.map((category) =>
+                    <div className="dropdown dropdown-hover">
+                        <Link to='signup'>
+                            <a className="bg-gray-300  text-gray-700 font-semibold py-2 px-4 rounded ">
+                                {category}
+                            </a>
+                        </Link>
                         <ul className="dropdown-content absolute hidden text-gray-700 pt-1">
                             {
                                 data.filter((item) => item?.category_name === category)
@@ -43,12 +46,12 @@ export default function NavbarItem() {
                                                 <ul className='dropdown-content dropdown-right absolute hidden text-gray-700 bg-gray-200 p-5 ml-32 -mt-6'>
                                                     <li>
                                                         {
-                                                        data.filter((item) => item.category_name === category && item.sub_category_name === subCategory)
-                                                         .map((item) => item.brand_name)
-                                                         .filter((brand) => brand)
-                                                         .map((brands) => (
-                                                            <li key={brands}>{brands}</li>
-                                                        ))
+                                                            data.filter((item) => item.category_name === category && item.sub_category_name === subCategory)
+                                                                .map((item) => item.brand_name)
+                                                                .filter((brandName, index, array) => brandName && array.indexOf(brandName) === index)
+                                                                .map((brands) => (
+                                                                    <li className='rounded-t bg-base-100 hover:bg-gray-400 block w-32 p-3' key={brands}>{brands}</li>
+                                                                ))
                                                         }
                                                     </li>
                                                 </ul>
