e3fbe0cc2116e68fe95469eaf2fafef260d547cc
marziamostafa <marziamostafa@gmail.com>
2023-11-05 20:57:53 +0600

    selecting options added in form

--- src/pages/AddProduct/AddProduct.tsx
+++ src/pages/AddProduct/AddProduct.tsx
@@ -1,10 +1,44 @@
 import { FieldValues, useForm } from "react-hook-form";
 import { AddProductValues } from "../../types/ProductTypes";
+import useApiData from "../../hooks/getAPIData";
 
 
 const AddProduct = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<AddProductValues>();
 
+    const { data, isLoading } = useApiData("http://localhost:5000/api/v1/allProducts")
+
+
+    const getCategory: Set<string> = new Set()
+    const getSubCategory: Set<string> = new Set()
+    const getBrand: Set<string> = new Set()
+
+    data.forEach(d => {
+        if (!getCategory.has(d.category_name)) {
+            getCategory.add(d.category_name);
+
+        }
+        if (!getSubCategory.has(d.sub_category_name)) {
+            getSubCategory.add(d.sub_category_name);
+        }
+        if (!getBrand.has(d.brand_name)) {
+            // console.log(d.brand_name);
+            getBrand.add(d.brand_name);
+        }
+    });
+
+    const getOneCategory = Array.from(getCategory);
+    const getOneSubCategory = Array.from(getSubCategory);
+    const getOneBrand = Array.from(getBrand);
+
+
+    // console.log(getOneCategory);
+    // console.log(getOneSubCategory);
+    // console.log(getOneBrand)
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
 
     const handleAddProduct = async (data: FieldValues) => {
 
@@ -21,23 +55,31 @@ const AddProduct = () => {
                 <form onSubmit={handleSubmit(handleAddProduct)}>
                     <div className="w-full max-w-xs form-control">
                         <label className="label"> <span className="label-text">Product category</span></label>
-                        <input type="text"
-                            {...register("category_name", {
-                                required: "Email Address is required"
-                            })}
-                            className="w-full max-w-xs input input-bordered" />
+                        <select className="select select-bordered  w-full max-w-xs" {...register("category_name")}>
+                            {
+                                getOneCategory.map(d => (
+                                    <option key={d} value={d}>{d}</option>
+                                ))
+                            }
+                        </select>
                         {errors.category_name && <p className='text-red-600'>{errors.category_name?.message}</p>}
                     </div>
 
                     {/* Sub-Category */}
                     <div className="w-full max-w-xs form-control">
-                        <label className="label"> <span className="label-text">Product Sub-category</span></label>
+                        <label className="label"> <span className="label-text">Brand Name</span></label>
 
-                        <input type="text" placeholder="Sub-Category"
-                            {...register("sub_category_name", {
 
-                            })}
-                            className="w-full max-w-xs input input-bordered" />
+                        <select className="select select-bordered  w-full max-w-xs" {...register("sub_category_name")}>
+                            {
+                                getOneSubCategory.map(d => (
+                                    <option key={d} value={d}>{d}</option>
+                                ))
+                            }
+                        </select>
+
+
+
                         {errors.sub_category_name && <p className='text-red-600'>{errors.sub_category_name?.message}</p>}
                     </div>
 
@@ -46,11 +88,13 @@ const AddProduct = () => {
                     <div className="w-full max-w-xs form-control">
                         <label className="label"> <span className="label-text">Brand Name</span></label>
 
-                        <input type="text" placeholder="Brand Name"
-                            {...register("brand_name", {
-
-                            })}
-                            className="w-full max-w-xs input input-bordered" />
+                        <select className="select select-bordered  w-full max-w-xs" {...register("brand_name")}>
+                            {
+                                getOneBrand.map(d => (
+                                    <option key={d} value={d}>{d}</option>
+                                ))
+                            }
+                        </select>
                         {errors.brand_name && <p className='text-red-600'>{errors.brand_name?.message}</p>}
                     </div>
                     {/* Product Name */}
