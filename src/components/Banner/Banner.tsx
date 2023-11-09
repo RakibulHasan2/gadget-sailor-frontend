ea597a636e015bb47e515b928997d1e75585e078
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-11-09 23:19:13 +0600

    banner updated

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,59 +5,6 @@ import img3 from '../../assets/banner/img3.png'
 const Banner = () => {
  
   return (
-    // <div className="mt-5 carousel">
-    //   <div id="slide1" className="relative carousel-item image-banner1">
-    //     <div className='flex items-center justify-center text-center'>
-    //       <img className='' src="https://www.startech.com.bd/image/cache/catalog/home/banner/Star-Tech-Laptop-Desktop-Deal-Home-Banner-982x500.png" alt="" />
-    //       {/* <div className=''>
-    //         <h1 className='text-6xl text-black '>Welcome to Gadget-Sailor</h1>
-    //         <div className='flex justify-center mt-10'>
-    //           <p className='w-2/4 text-black'>
-    //             "Discover [Gadget-sailor], your trusted source for top-tier PCs and laptops. We offer a curated selection of cutting-edge technology and accessories to meet your computing needs. Our expert team is dedicated to providing personalized solutions, ensuring a seamless and satisfying shopping experience for all tech enthusiasts."
-    //           </p>
-    //         </div>
-    //       </div> */}
-    //     </div>
-    //     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 ">
-    //       <a href="#slide3" className="btn btn-circle">❮</a>
-    //       <a href="#slide2" className="btn btn-circle">❯</a>
-    //     </div>
-    //   </div>
-    //   <div id="slide2" className="relative w-full h-screen carousel-item image-banner2">
-    //     <div className='flex items-center justify-center text-center'>
-    //     <img className='' src="https://www.startech.com.bd/image/cache/catalog/home/banner/Star-Tech-Laptop-Desktop-Deal-Home-Banner-982x500.png" alt="" />
-    //       {/* <div className=''>
-    //         <h1 className='text-5xl text-black '>Welcome to Gadget-Sailor</h1>
-    //         <div className='flex justify-center mt-10'>
-    //           <p className='w-2/4 text-black'>
-    //             "Discover [Gadget-sailor], your trusted source for top-tier PCs and laptops. We offer a curated selection of cutting-edge technology and accessories to meet your computing needs. Our expert team is dedicated to providing personalized solutions, ensuring a seamless and satisfying shopping experience for all tech enthusiasts."
-    //           </p>
-    //         </div>
-    //       </div> */}
-    //     </div>
-    //     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-    //       <a href="#slide1" className="btn btn-circle">❮</a>
-    //       <a href="#slide3" className="btn btn-circle">❯</a>
-    //     </div>
-    //   </div>
-    //   <div id="slide3" className="relative w-full h-screen carousel-item image-banner3">
-    //     <div className='flex items-center justify-center text-center'>
-    //     <img className='' src="https://www.startech.com.bd/image/cache/catalog/home/banner/Star-Tech-Laptop-Desktop-Deal-Home-Banner-982x500.png" alt="" />
-    //       {/* <div className=''>
-    //         <h1 className='text-5xl text-black '>Welcome to Gadget-Sailor</h1>
-    //         <div className='flex justify-center mt-10'>
-    //           <p className='w-2/4 text-black'>
-    //             "Discover [Gadget-sailor], your trusted source for top-tier PCs and laptops. We offer a curated selection of cutting-edge technology and accessories to meet your computing needs. Our expert team is dedicated to providing personalized solutions, ensuring a seamless and satisfying shopping experience for all tech enthusiasts."
-    //           </p>
-    //         </div>
-    //       </div> */}
-    //     </div>
-    //     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-    //       <a href="#slide2" className="btn btn-circle">❮</a>
-    //       <a href="#slide1" className="btn btn-circle">❯</a>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="carousel w-full mt-5">
       <div id="slide1" className="carousel-item relative w-full">
         <img src={img1} className="w-full" />
