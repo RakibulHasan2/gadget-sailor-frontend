0f131a932f2f3b43486872d2f35aef2fe93ee990
omarfarukee <umarfaruque1920@gmail.com>
2023-11-09 21:02:08 +0600

    updated banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -6,7 +6,11 @@ const Banner = () => {
   return (
     <div className="w-full carousel">
       <div id="slide1" className="relative w-screen h-screen carousel-item image-banner1">
-        <></>
+          <div>
+            <div>
+              <h1>helcome to Gadget-Sailor</h1>
+            </div>
+          </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 ">
           <a href="#slide3" className="btn btn-circle">❮</a>
           <a href="#slide2" className="btn btn-circle">❯</a>
