82f13f98a064c2d73f7cf8633a5978553b2fdfb3
omarfarukee <umarfaruque1920@gmail.com>
2023-11-10 15:42:16 +0600

    my profile user data loaded

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -7,8 +7,8 @@ const user  = JSON.parse(userData as string)
 console.log(user)
   return (
     <div>
-      <h1>this is my profile</h1>
-      <h1>this is my profile{user.name.firstName}</h1>
+      <h1>this is my profile{user.email}</h1>
+      <h1>this is my profile{user.name.firstame}</h1>
     </div>
   )
 }
