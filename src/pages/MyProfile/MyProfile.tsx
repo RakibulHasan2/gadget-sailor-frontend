4686708829fc95c8457fa5dfef1721a5607e056e
omarfarukee <umarfaruque1920@gmail.com>
2023-11-10 15:41:50 +0600

    my profile page route set up done

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -8,7 +8,7 @@ console.log(user)
   return (
     <div>
       <h1>this is my profile</h1>
-      <h1>this is my profile</h1>
+      <h1>this is my profile{user.name.firstName}</h1>
     </div>
   )
 }
