6bcec4332bef1add887b4b2581f47b2e403e4fb4
omarfarukee <umarfaruque1920@gmail.com>
2023-11-14 20:13:25 +0600

    card updated

--- src/components/Products/ProductsCard.tsx
+++ src/components/Products/ProductsCard.tsx
@@ -21,19 +21,23 @@ export default function ProductsCard({ product }: IProduct) {
                 </div>
             </Link>
         </div> */}
-            <div className="relative w-full transition-transform transform border hover:shadow-2xl rounded-xl card h-96 bg-base-100 hover:scale-105">
-                <figure className="px-10 pt-10">
+
+            <div className="relative w-full overflow-hidden transition-all duration-300 border border-transparent shadow-md card h-96 bg-base-100 hover:border-blue-400 hover:shadow-customBlue">
+                <figure className="px-10 pt-10 transition-transform transform hover:scale-110">
                     <img src="https://globalexpressbd.com/wp-content/uploads/2021/10/i7-11700k-gaming-pc-500x500-1.jpg" alt="Shoes" className="rounded-xl" />
                 </figure>
-                <Link to={`/product/${_id}`}>
+               
                     <div className="items-center text-center card-body">
+                         <Link to={`/product/${_id}`}>
                         <p>{product_name}</p>
+                           </Link>
                         <div className="card-actions">
                             <p className="font-bold text-yellow-700">{price}৳</p>
                         </div>
                     </div>
-                </Link>
+             
             </div>
+
 
         </div>
 
