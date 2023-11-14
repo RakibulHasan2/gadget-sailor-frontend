d534ebab2ebcb981a770e400b18716e8f5b1119c
omarfarukee <umarfaruque1920@gmail.com>
2023-11-14 21:23:42 +0600

    card updated

--- src/components/Products/ProductsCard.tsx
+++ src/components/Products/ProductsCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import '../../styles/ProductsCard.css'
 import { IProduct } from '../../types/ProductsType';
+import { FaCartPlus } from 'react-icons/fa';
 
 
 export default function ProductsCard({ product }: IProduct) {
@@ -22,17 +23,20 @@ export default function ProductsCard({ product }: IProduct) {
             </Link>
         </div> */}
 
-            <div className="relative w-full overflow-hidden transition-all duration-300 border border-transparent shadow-md card h-96 bg-base-100 hover:border-blue-400 hover:shadow-customBlue">
+            <div className="relative w-full overflow-hidden transition-all duration-300 border border-transparent shadow-md card bg-base-100 hover:border-blue-400 hover:shadow-customBlue card-heigth">
                 <figure className="px-10 pt-10 transition-transform transform hover:scale-110">
                     <img src="https://globalexpressbd.com/wp-content/uploads/2021/10/i7-11700k-gaming-pc-500x500-1.jpg" alt="Shoes" className="rounded-xl" />
                 </figure>
                
                     <div className="items-center text-center card-body">
                          <Link to={`/product/${_id}`}>
-                        <p>{product_name}</p>
+                         <p className='w-full h-20 font-bold border-b-2 hover:text-blue-700'>{product_name}</p>
                            </Link>
-                        <div className="card-actions">
+                        <div className="mt-5 card-actions">
                             <p className="font-bold text-yellow-700">{price}৳</p>
+                        </div>
+                        <div>
+                            <button className='flex items-center justify-center p-2 text-lg text-blue-700 bg-gray-100 rounded-md gap-x-3 w-44 hover:bg-blue-700 hover:text-white'><FaCartPlus></FaCartPlus> Buy now</button>
                         </div>
                     </div>
              
