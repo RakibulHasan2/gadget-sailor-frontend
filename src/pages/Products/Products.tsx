c695790cd2c9cfd10ae547b9973cb4bb1f8458ca
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-11-15 11:34:26 +0600

    font style changed

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -108,7 +108,7 @@ export default function Products() {
                         products?.map((product: IProduct) =>
                             <ProductsCard
                                 key={product._id}
-                                product={product} />
+                                product={product} _id={""} category_name={""} sub_category_name={""} brand_name={""} product_name={""} image={[]} model={""} description={""} price={0} product_code={0} status={""} reviews={[]} warranty={""} __v={""} others_info={[]} />
                         )
                     }
                 </div>
