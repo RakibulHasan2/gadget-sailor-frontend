f46565a7384bde242e4ef8c78aecf2c168dae69e
omarfarukee <umarfaruque1920@gmail.com>
2023-11-15 09:01:56 +0600

    edit profile and my profile updated

--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -2,6 +2,7 @@
 import { FieldValues, useForm } from "react-hook-form";
 import { IUpdateUsers } from "../../types/MyProfileType";
 import toast from "react-hot-toast";
+import { FcEditImage } from 'react-icons/fc';
 
 export default function EditProfile() {
   const { register, handleSubmit } = useForm<IUpdateUsers>();
@@ -12,94 +13,102 @@ export default function EditProfile() {
   const imageHostKey = '29473dd4ab78ebc95009722bc0558d38';
   console.log(imageHostKey)
 
+  // const handleAddImage = async (data: FieldValues) => {
+  //   console.log(data)
+
+  //   const image = data.image[0];
+  //   const fromData = new FormData();
+  //   fromData.append('image', image);
+
+  //   const url = `https://api.imgbb.com/1/upload?&key=${imageHostKey}`
+  //   console.log(url)
+  //   fetch(url, {
+  //     method: 'POST',
+  //     body: fromData
+  //   })
+  //     .then(res => res.json())
+  //     .then(async imgData => {
+  //       if (imgData.success) {
+
+
+  //         console.log(imgData.data.url)
+
+
+  //         const upload: IUpdateUsers = {
+
+  //           image: imgData.data.url
+
+  //         }
+
+
+  //         const response = await fetch(`http://localhost:5000/api/v1/users/${user._id}`, {
+  //           method: 'PUT',
+  //           headers: {
+  //             'Content-Type': 'application/json',
+  //           },
+  //           body: JSON.stringify(upload),
+  //         });
+
+  //         const myProfileImage = await response.json();
+  //         if (myProfileImage.statusCode === 200) {
+
+  //           toast.success("successfully uploaded")
+  //         } else {
+  //           toast.error("failed to upload")
+  //         }
+
+  //       }
+  //     })
+  // }
   const handleAddImage = async (data: FieldValues) => {
-    console.log(data)
+    console.log(data);
 
     const image = data.image[0];
-    const fromData = new FormData();
-    fromData.append('image', image);
+    const formData = new FormData();
+    formData.append('image', image);
 
-    const url = `https://api.imgbb.com/1/upload?&key=${imageHostKey}`
-    console.log(url)
-    fetch(url, {
-      method: 'POST',
-      body: fromData
-    })
-      .then(res => res.json())
-      .then(async imgData => {
-        if (imgData.success) {
+    const url = `https://api.imgbb.com/1/upload?&key=${imageHostKey}`;
+    console.log(url);
 
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: formData,
+      });
 
-          console.log(imgData.data.url)
+      const imgData = await response.json();
 
+      if (imgData.success) {
+        console.log(imgData.data.url);
 
-          const upload: IUpdateUsers = {
+        const upload: IUpdateUsers = {
+          image: imgData.data.url,
+        };
 
-            image: imgData.data.url
+        const updateResponse = await fetch(`http://localhost:5000/api/v1/users/${user._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(upload),
+        });
 
-          }
+        const myProfileImage = await updateResponse.json();
 
+        if (myProfileImage.statusCode === 200) {
+          const updatedUserData = { ...user, image: imgData.data.url };
+          sessionStorage.setItem('userData', JSON.stringify(updatedUserData));
 
-          const response = await fetch(`http://localhost:5000/api/v1/users/${user._id}`, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(upload),
-          });
-
-          const myProfileImage = await response.json();
-          if (myProfileImage.statusCode === 200) {
-
-            toast.success("successfully uploaded")
-          } else {
-            toast.error("failed to upload")
-          }
-
+          toast.success('Successfully uploaded');
+        } else {
+          toast.error('Failed to upload');
         }
-      })
-  }
+      }
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  };
 
-
-
-  // const handleUpdateProfile = async (data: FieldValues) => {
-
-
-  //   const userprofileData: IUpdateUsers = {
-  //     name: {
-  //       firstName: data.name.firstName,
-  //       lastName: data.name.lastName,
-  //     },
-  //     email: data.email,
-  //     password: data.password,
-  //     phoneNumber: data.phoneNumber,
-  //     present_address: data.present_address,
-  //     permanent_address: data.permanent_address,
-  //     post_code: data.post_code,
-  //     city: data.city,
-  //     division: data.division,
-
-  //   };
-
-  //   console.log(userprofileData)
-
-  //   const response = await fetch(`http://localhost:5000/api/v1/users/${user._id}`, {
-  //     method: 'PUT',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(userprofileData),
-  //   });
-
-  //   const myProfile = await response.json();
-  //   if (myProfile.statusCode === 200) {
-
-  //     toast.success('updated profile successfully')
-  //   } else {
-  //     toast.error('error updating profile')
-  //   }
-
-  // };
 
   const handleUpdateProfile = async (data: FieldValues) => {
     const userprofileData: IUpdateUsers = {
@@ -116,9 +125,9 @@ export default function EditProfile() {
       city: data.city,
       division: data.division,
     };
-  
+
     console.log(userprofileData);
-  
+
     try {
       const response = await fetch(`http://localhost:5000/api/v1/users/${user._id}`, {
         method: 'PUT',
@@ -127,14 +136,14 @@ export default function EditProfile() {
         },
         body: JSON.stringify(userprofileData),
       });
-  
+
       const myProfile = await response.json();
-      
+
       if (myProfile.statusCode === 200) {
-        
+
         const updatedUserData = JSON.stringify(myProfile.data);
         sessionStorage.setItem('userData', updatedUserData);
-  
+
         toast.success('Updated profile successfully');
       } else {
         toast.error('Error updating profile');
@@ -144,121 +153,138 @@ export default function EditProfile() {
       toast.error('Failed to update profile');
     }
   };
-  
+
   return (
     <div>
-
-
-      <form onSubmit={handleSubmit(handleAddImage)}>
-
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">Photo (photo Should be png/jpg format)</span></label>
-          <input type="file" multiple {...register("image", {
-          })} className="w-full max-w-xs input input-bordered" />
-        </div>
-        <input className="mt-4 bg-blue-600 w-28 btn" value="Update" type="submit" />
-      </form>
-
-
-
       <form onSubmit={handleSubmit(handleUpdateProfile)}>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">First Name</span></label>
-          <input
-            type="text"
-            placeholder="First Name"
-            {...register("name.firstName", {})}
-            className="w-full max-w-xs input input-bordered"
-            defaultValue={user?.name.firstName}
-          />
+        <div className="flex ">
+          <div className="w-full">
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">First Name</span></label>
+              <input
+                type="text"
+                placeholder="First Name"
+                {...register("name.firstName", {})}
+                className="w-full max-w-xs input input-bordered rounded-3xl"
+                defaultValue={user?.name.firstName}
+              />
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">Last Name</span></label>
+
+              <input
+                type="text"
+                placeholder="Last Name"
+                {...register("name.lastName", {})}
+                className="w-full max-w-xs input input-bordered rounded-3xl"
+                defaultValue={user?.name.lastName}
+              />
+
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">Email</span></label>
+
+              <input type="email" placeholder="email"
+                {...register("email", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user.email} disabled />
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">Password</span></label>
+
+              <input type="text" placeholder="password"
+                {...register("password", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user?.password} />
+
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">phoneNumber</span></label>
+
+              <input type="text" placeholder="phoneNumber"
+                {...register("phoneNumber", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user?.phoneNumber} />
+
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">present_address</span></label>
+
+              <input type="text" placeholder="present address"
+                {...register("present_address", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user?.present_address} />
+
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">permanent address</span></label>
+
+              <input type="text" placeholder="permanent address"
+                {...register("permanent_address", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user?.permanent_address} />
+
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">post code</span></label>
+
+              <input type="text" placeholder="post code"
+                {...register("post_code", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user?.post_code} />
+
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">Division</span></label>
+
+              <input type="text" placeholder="division"
+                {...register("division", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user?.division} />
+
+            </div>
+            <div className="w-full max-w-xs form-control">
+              <label className="label"> <span className="label-text">city</span></label>
+
+              <input type="text" placeholder="city"
+                {...register("city", {
+                })}
+                className="w-full max-w-xs input input-bordered rounded-3xl" defaultValue={user?.city} />
+
+            </div>
+          </div>
         </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">Last Name</span></label>
+        <input className="w-40 mt-4 text-white bg-blue-600 hover:text-black btn rounded-3xl" value="Update" type="submit" />
 
-          <input
-            type="text"
-            placeholder="Last Name"
-            {...register("name.lastName", {})}
-            className="w-full max-w-xs input input-bordered"
-            defaultValue={user?.name.lastName}
-          />
-
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">Email</span></label>
-
-          <input type="email" placeholder="email"
-            {...register("email", {
-            })}
-            className="w-full max-w-xs input input-bordered" defaultValue={user.email} disabled/>
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">Password</span></label>
-
-          <input type="text" placeholder="password"
-            {...register("password", {
-            })}
-            className="w-full max-w-xs input input-bordered" defaultValue={user?.password} />
-
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">phoneNumber</span></label>
-
-          <input type="text" placeholder="phoneNumber"
-            {...register("phoneNumber", {
-            })}
-            className="w-full max-w-xs input input-bordered" defaultValue={user?.phoneNumber} />
-
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">present_address</span></label>
-
-          <input type="text" placeholder="present address"
-            {...register("present_address", {
-            })}
-            className="w-full max-w-xs input input-bordered"  defaultValue={user?.present_address}/>
-
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">permanent address</span></label>
-
-          <input type="text" placeholder="permanent address"
-            {...register("permanent_address", {
-            })}
-            className="w-full max-w-xs input input-bordered" defaultValue={user?.permanent_address}/>
-
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">post code</span></label>
-
-          <input type="text" placeholder="post code"
-            {...register("post_code", {
-            })}
-            className="w-full max-w-xs input input-bordered" defaultValue={user?.post_code}/>
-
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">Division</span></label>
-
-          <input type="text" placeholder="division"
-            {...register("division", {
-            })}
-            className="w-full max-w-xs input input-bordered" defaultValue={user?.division} />
-
-        </div>
-        <div className="w-full max-w-xs form-control">
-          <label className="label"> <span className="label-text">city</span></label>
-
-          <input type="text" placeholder="city"
-            {...register("city", {
-            })}
-            className="w-full max-w-xs input input-bordered" defaultValue={user?.city}/>
-
-        </div>
-
-        <input className="w-40 mt-4 bg-blue-600 btn" value="Update" type="submit" />
-        <div></div>
       </form>
+      <div>
+
+        <div className="flex justify-center mb-2 border-b-8">
+          <div className=''>
+            <FcEditImage className="ml-5 text-6xl"> </FcEditImage>
+            <h1 className="font-bold">Edit Profile</h1>
+          </div>
+
+        </div>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit(handleAddImage)} className="flex justify-around mb-10">
+
+          <div className="w-full max-w-xs form-control">
+            <label className="label"> <span className="label-text">Photo (photo Should be png/jpg format)</span></label>
+            <input type="file" multiple {...register("image", {
+
+            })} className="w-full max-w-xs input input-bordered rounded-3xl" />
+            {/* {errors.image && <p className='text-red-500'>please select image file</p>} */}
+          </div>
+          <div className="mt-9">
+            <input className="text-white bg-blue-600 hover:text-black btn rounded-3xl" value="Upload" type="submit" />
+          </div>
+
+        </form>
+      </div>
     </div>
   );
 }
