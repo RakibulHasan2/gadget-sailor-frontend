704f298e72061b32e07b5fccbb16b0136b53cb04
marziamostafa <marziamostafa@gmail.com>
2023-11-18 03:38:15 +0600

    tested

--- src/components/RandomProducts/RandomProducts.tsx
+++ src/components/RandomProducts/RandomProducts.tsx
@@ -4,46 +4,29 @@ import RandomProductCard from "./RandomProductCard";
 
 
 
-export default function RandomProducts() {
+const RandomProducts = () => {
     const [page, setPage] = useState(0);
-    const [size, setSize] = useState(8);
-    const [count, setCount] = useState(0)
 
+    const [count, setCount] = useState(0)
+    const size = 8;
     const { data, isLoading } = useApiData("http://localhost:5000/api/v1/allProducts")
     if (isLoading) {
         return <p>Loading...</p>;
     }
-    // console.log(data.length)
 
-    // const [page, setPage] = React.useState<number>(1);
-
-    // const [count, setCount] = React.useState<number>(0);
-
-
-
-
-
-
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
 
 
     console.log(data)
 
 
     const pages = Math.ceil(data.length / size);
-    //console.log(pages)
+    console.log(page)
 
     const randomDatas = [...data].sort(() => Math.random() - 0.5);
     //console.log(randomDatas.length)
 
     const limitData = randomDatas.slice(count * size, page * size)
     console.log(limitData)
-
-    // const number: number[] = [...Array(pages).keys()];
-    // number.map(num => {
-    //     console.log(num)
-    // })
 
     return (
         <div>
@@ -56,42 +39,20 @@ export default function RandomProducts() {
                     ))
                 }
             </div>
-            <div className="btn-group flex justify-center pagination">
-                <p>Page no:{page}</p>
-                {[...Array(pages).keys()].map((number) => (
-                    <button
+
+            <div className="btn-group flex justify-center pagination ">
+                {
+                    [...Array(pages).keys()].map(number => <button
                         key={number}
-                        onClick={() => {
-                            setPage(number + 1);
-                            setCount(number)
-                        }}
-                        className={page === number + 1 ? "btn btn-primary" : "btn"}
-                    >
-                        {number + 1}
-                    </button>
-                ))}
+
+                        onClick={() => { setPage(number + 1); setCount(number) }}
+                        className={page === number + 1 ? "btn btn-primary" : "btn"}>{number + 1}</button>)
+
+                }
+
             </div>
-
-            {/* <div className="join">
-                <button className={page === number - 1 ? "join-item btn" : "btn"} onClick={() => {
-                    setPage(number - 1); setCount(number)
-                }}>«</button>
-
-                <button className="join-item btn">{number}</button>
-                <button className={page === number + 1 ? "join-item btn" : "btn"} onClick={() => {
-                    setPage(number + 1); setCount(number)
-                }}>»</button>
-            </div> */}
-
-            {/* <div>
-                <div className="join">
-                    <button className="join-item btn">«</button>
-                    <button className="join-item btn">Page 22</button>
-                    <button className="join-item btn">»</button>
-                </div>
-            </div> */}
         </div>
     );
-}
+};
 
-//export default RandomProducts;
+export default RandomProducts;
