4bbf22c52c7d2fc2b158eeb81d227b87dc5a8212
omarfarukee <umarfaruque1920@gmail.com>
2023-11-18 11:35:21 +0600

    hot offer page created

---
+++ src/components/HotOffer/HotOffer.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function HotOffer() {
+    return (
+        <div className="flex justify-center p-16 mb-28">
+            <div>
+                <h1 className="text-5xl text-blue-700">hot Hot offer comming soon.....,</h1>
+                <h1 className="mt-5 text-3xl">stay with rakibs the Highly logical thinker,</h1> 
+                <h1 className="text-3xl">Marzia the Complex problem solver,</h1>
+                <h1 className="text-3xl">porosh the hudai asi 🙂</h1>
+            </div>
+
+        </div>
+    )
+}
