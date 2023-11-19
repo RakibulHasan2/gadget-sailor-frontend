71737007519177a93f8f131a61672b5634c37ec8
marziamostafa <marziamostafa@gmail.com>
2023-11-19 19:42:08 +0600

    modal added

---
+++ src/pages/UpdateProduct/UpdateModal.tsx
@@ -0,0 +1,13 @@
+import { IProduct } from "../../types/ProductsType";
+
+
+const UpdateModal = ({ data }: IProduct) => {
+    console.log(data)
+    return (
+        <div>
+            it is update modal
+        </div>
+    );
+};
+
+export default UpdateModal;
