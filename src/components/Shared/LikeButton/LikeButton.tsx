3e32cf5ef1a39d77f76c4816f556cd5f8123c084
marziamostafa <marziamostafa@gmail.com>
2023-11-21 12:35:51 +0600

    like button worked

--- src/components/Shared/LikeButton/LikeButton.tsx
+++ src/components/Shared/LikeButton/LikeButton.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 
 
 const LikeButton = () => {
+    const [liked, setLiked] = useState(false);
+    const handleLike = () => {
+        if (liked) {
+            setLiked(false)
+        }
+        else {
+            setLiked(true)
+        }
+    }
+
+
     return (
         <div>
-            <button className="" >
+            <button className="" title={liked ? "" : "add to favourite"}
+                onClick={handleLike}
+            >
 
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="red" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill={liked ? "red" : "none"} viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
             </button>
         </div>
     );
