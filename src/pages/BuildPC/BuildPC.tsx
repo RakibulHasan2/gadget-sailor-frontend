fd44662bfb3832e9791f0b9a89cfc91c79db50e9
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-11-22 20:01:09 +0600

    cart tofixed function error solve

--- src/pages/BuildPC/BuildPC.tsx
+++ src/pages/BuildPC/BuildPC.tsx
@@ -1,8 +1,14 @@
+import { BsCpu } from "react-icons/bs";
 
 export default function BuildPC() {
     return (
-        <div>
+        <div className="border mt-10 p-10">
             <h1>BUILD PC</h1>
+            <div className="border flex justify-between mt-5">
+                <BsCpu className = 'text-3xl text-blue-900'></BsCpu>
+                <h1 className="font-bold text-xl">CPU</h1>
+                <button type="button" className="p-3 bg-blue-600 text-white rounded-xl hover:bg-sky-700">Choose</button>
+            </div>
         </div>
     )
 }
