26c666a1a8d9672bc70c437f070deed7880a9860
marziamostafa <marziamostafa@gmail.com>
2023-11-24 22:54:46 +0600

    css added

--- src/components/AutoCardCarousel/AutoCardCarousel.tsx
+++ src/components/AutoCardCarousel/AutoCardCarousel.tsx
@@ -1,21 +1,36 @@
 import useFavData from "../../hooks/getFavData";
-import AutoSingleCard from "./AutoSingleCard";
+import './AutoCardCarousel.css'
+import SingleCards from "./SingleCards";
+import { useEffect, useState } from "react";
 
 
 const AutoCardCarousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
     const { data } = useFavData(`http://localhost:5000/api/v1/getFav`)
     console.log(data)
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [data.length]);
+
+    const translateValue = -currentIndex * 100 + '%';
+
     return (
         <div className="carousel-container">
-            <div className="cards-wrapper">
-                {
-                    data.map(d => (
-                        <AutoSingleCard key={d._id} />
-                    ))
-                }
-            </div>
-        </div>
+            <div className="cards-wrapper" style={{ transform: `translateX(${translateValue})` }}></div>
+            {
+                data.map(d => (
+                    <SingleCards
+                        key={d._id}
+                        data={d} _id={""} category_name={""} sub_category_name={""} brand_name={""} product_name={""} image={[]} model={""} description={""} price={0} product_code={0} status={""} reviews={[]} warranty={""} __v={""} others_info={[]}                    ></SingleCards>
+                ))
+            }
+
+        </div >
     );
 };
 
---
+++ src/components/AutoCardCarousel/SingleCards.tsx
@@ -0,0 +1,15 @@
+import { IProduct } from "../../types/ProductsType";
+
+
+const SingleCards = ({ data }: IProduct) => {
+    console.log(data)
+    return (
+        <div className="card">
+            <img src={data.image} alt={`Card ${data._id}`} />
+            {/* <h3>{card.title}</h3>
+            <p>{card.description}</p> */}
+        </div>
+    );
+};
+
+export default SingleCards;
