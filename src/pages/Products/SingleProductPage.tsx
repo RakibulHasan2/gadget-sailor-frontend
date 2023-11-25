98ab82bf0930d996f8c3f3fd1d95a6e34b6b6292
marziamostafa <marziamostafa@gmail.com>
2023-11-25 19:05:09 +0600

    ups and monitor related data showed

--- src/pages/Products/SingleProductPage.tsx
+++ src/pages/Products/SingleProductPage.tsx
@@ -103,13 +103,24 @@ export default function SingleProductPage() {
 
 
   useEffect(() => {
-    fetch(`http://localhost:5000/api/v1/allProducts/${sub_category_name}`)
-      .then(res => res.json())
-      .then(data => {
-        //console.log(data.data)
-        setSuggestedData(data.data)
-      })
-  }, [sub_category_name])
+    if (category_name === 'UPS' || category_name === 'Monitor') {
+      fetch(`http://localhost:5000/api/v1/allProducts/${category_name}`)
+        .then(res => res.json())
+        .then(data => {
+          //console.log(data.data)
+          setSuggestedData(data.data)
+        })
+    }
+
+    else {
+      fetch(`http://localhost:5000/api/v1/allProducts/${sub_category_name}`)
+        .then(res => res.json())
+        .then(data => {
+          //console.log(data.data)
+          setSuggestedData(data.data)
+        })
+    }
+  }, [category_name, sub_category_name])
 
   const randomSuggestion = [...suggestedData].sort(() => Math.random() - 0.5)
   const limitData = randomSuggestion.slice(0, 5)
