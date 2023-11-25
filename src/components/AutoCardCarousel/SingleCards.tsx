4a63a973fc4a9c52aa7167a3d96608373b4bf613
marziamostafa <marziamostafa@gmail.com>
2023-11-25 17:22:22 +0600

    animated fav done

--- src/components/AutoCardCarousel/SingleCards.tsx
+++ src/components/AutoCardCarousel/SingleCards.tsx
@@ -6,13 +6,13 @@ const SingleCards = ({ data }: IProduct) => {
     console.log(data)
     return (
         <div>
-            <div className=" w-60 max-h-64 border border-transparent shadow-md card bg-base-100 hover:border-blue-400 hover:shadow-customBlue card-height rounded-xl">
-                <figure className="h-full px-10 transition-transform transform hover:scale-110">
+            <div className=" w-60 max-h-64 border border-transparent shadow-md card bg-base-100 hover:border-blue-400 hover:shadow-customBlue card-height rounded-xl mr-10 ">
+                <figure className="h-full px-10 mt-4 transition-transform transform hover:scale-110">
                     <img src={data?.image} alt="" className="rounded-xl" />
                 </figure>
 
-                <div className="items-center text-center card-body">
-                    <Link to={`/product/${data?._id}`}>
+                <div className="items-center text-center card-body py-0">
+                    <Link to={`/product/${data?.I_id}`}>
                         <p className='w-full h-16 font-bold  hover:text-blue-700'>{data?.product_name}</p>
                     </Link>
                     <p className="font-bold text-yellow-700">{data?.price}৳</p>
