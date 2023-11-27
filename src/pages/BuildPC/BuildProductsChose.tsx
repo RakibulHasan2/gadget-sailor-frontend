843d2f25202aa5027fd1847907f63b0efbb408cd
omarfarukee <umarfaruque1920@gmail.com>
2023-11-27 22:26:40 +0600

    ...

--- src/pages/BuildPC/BuildProductsChose.tsx
+++ src/pages/BuildPC/BuildProductsChose.tsx
@@ -25,7 +25,7 @@ export default function BuildProductsChose() {
 
   return (
     <div className="justify-between p-5 lg:flex">
-      <div className="p-2 w-60 bg-slate-100 ">
+      <div className="p-3 rounded-lg lg:p-2 lg:w-60 bg-slate-100 lg:rounded-none">
         <div>
           <input
             type="text"
@@ -36,24 +36,24 @@ export default function BuildProductsChose() {
           />
         </div>
       </div>
-      <div className="w-4/5 mt-5">
+      <div className="mt-5 lg:w-4/5">
         {filteredProducts.length > 0 ? (
           filteredProducts.map((product: IProduct) => (
-            <div key={product._id} className="grid grid-cols-3 p-4 mb-3 bg-white border rounded-lg shadow-md">
-              <div className="border-r-2 mr-36 ">
-                <img className="w-36" src={product?.image[0]} alt="" />
+            <div key={product._id} className="grid-cols-3 p-4 mb-3 bg-white border rounded-lg shadow-md lg:grid">
+              <div className="flex justify-center lg:flex-none lg:border-r-2 lg:mr-36">
+                <img className="w-40 lg:w-52" src={product?.image[0]} alt="" />
               </div>
               <div className="items-center">
                 <Link to={`/product/${product?._id}`}>
-                  <h2 className="w-full text-xl font-bold hover:text-blue-700">{product.product_name}</h2>
+                  <h2 className="w-full text-xl font-bold text-center lg:text-left hover:text-blue-700">{product.product_name}</h2>
                 </Link>
                 <li className="mt-4"><span className="font-bold">Model-</span> {product.model}</li>
                 <li className=""><span className="font-bold">Price-</span> {product.price}৳</li>
                 <li className=""><span className="font-bold">Warranty-</span> {product.warranty}</li>
               </div>
-              <div className="flex items-center justify-end pr-10 ">
+              <div className="flex justify-center lg:pr-10 ustify-center lg:justify-end ">
                 <Link to='/build-pc'>
-                  <button onClick={() => handleAddToBuild(product)} className="flex items-center justify-center p-2 mt-12 text-lg font-bold bg-gray-200 w-36 gap-x-2 rounded-2xl hover:bg-blue-800 hover:text-white">
+                  <button onClick={() => handleAddToBuild(product)} className="flex items-center justify-center p-2 mt-10 text-lg font-bold bg-gray-200 lg:mt-20 w-36 gap-x-2 rounded-2xl hover:bg-blue-800 hover:text-white ">
                     <BiSolidMessageAltAdd />Add
                   </button>
                 </Link>
@@ -62,7 +62,7 @@ export default function BuildProductsChose() {
           ))
         ) : (
           <div className="flex justify-center p-40 font-bold">
-            <span className="loader-pc"></span>
+            <span className="text-center lg:text-3xl loader-pc"></span>
           </div>
         )}
       </div>
