b050616476e9a8472e387a95a626f38b6cc1f2e4
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-11-27 16:48:38 +0600

    added banner, parallax and whyGadgetSailor components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,18 @@
 import AutoCardCarousel from "../../components/AutoCardCarousel/AutoCardCarousel";
 import Banner from "../../components/Banner/Banner";
+import Parallax from "../../components/Parallax/Parallax";
 import RandomProducts from "../../components/RandomProducts/RandomProducts";
+import WhyGadgetSailor from "../../components/WhyGadgetSailor/WhyGadgetSailor";
 
 
 export default function Home() {
   return (
     <div>
+      {/* Fahim has started Fahimming */}
       <Banner></Banner>
       <RandomProducts></RandomProducts>
+      <WhyGadgetSailor />
+      <Parallax />
       <AutoCardCarousel />
     </div>
   )
