60b7b590dfcbe916c21e5dd52fec94eaa4527b25
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-11-28 17:34:02 +0600

    home page modal created

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,12 @@ import Banner from "../../components/Banner/Banner";
 import Parallax from "../../components/Parallax/Parallax";
 import RandomProducts from "../../components/RandomProducts/RandomProducts";
 import WhyGadgetSailor from "../../components/WhyGadgetSailor/WhyGadgetSailor";
+import Modal from "../../components/Modal";
 
 export default function Home() {
   return (
     <div>
+      <Modal />
       <Banner></Banner>
       <AutoCardCarousel />
       <Parallax />
