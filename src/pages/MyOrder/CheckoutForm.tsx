f47acb3ecf182ae7f69e03da3fcee6b6a9257079
marziamostafa <marziamostafa@gmail.com>
2023-12-01 00:24:48 +0600

    post in stripe

--- src/pages/MyOrder/CheckoutForm.tsx
+++ src/pages/MyOrder/CheckoutForm.tsx
@@ -1,13 +1,26 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 //import { StripeError } from "@stripe/stripe-js";
-//import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CheckoutForm = ({ data }: any) => {
+
     console.log(data)
+    const [clientSecret, setClientSecret] = useState("");
     // const [cardError, setCardError] = useState<string | null>(null)
     const stripe = useStripe();
     const elements = useElements();
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:5000/api/v1/create-payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, []);
 
 
     const handleSubmit = async (event: { preventDefault: () => void; }) => {
