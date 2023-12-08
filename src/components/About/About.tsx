46a4734f89b3ed5b2bfcf79a5a4206afad1fa384
omarfarukee <umarfaruque1920@gmail.com>
2023-12-08 18:57:28 +0600

    ...

---
+++ src/components/About/About.tsx
@@ -0,0 +1,13 @@
+
+
+export default function About() {
+  return (
+    <div>
+        <div>
+            <div className="flex justify-center p-4 text-3xl">
+                <h1>About us</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
